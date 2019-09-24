080d04f9722c21bb880cd9d518c1786b01425399
Nick Palacio <nick.palacio@fcsamerica.com>
2019-09-24 14:52:48 -0500

    Got question 1 working

--- AdvancedConceptsInProgrammingLanguages/HW2/HW2.pl
+++ AdvancedConceptsInProgrammingLanguages/HW2/HW2.pl
@@ -8,65 +8,37 @@ member(X, [Y|Tail]) :-
     different(X,Y),
     member(X, Tail).
 
-% remove_duplicates([],[]).
+% I stole remove_duplicates from a stack overflow question (https://stackoverflow.com/questions/39435709/how-to-remove-duplicates-from-a-list-in-swi-prolog)
+% but I will annotate what this is doing
+% remove_duplicates/2
+% 'The list after removing duplicates from an empty list is an empty list'
+remove_duplicates([],[]).
 
-% remove_duplicates([H | T], List) :-    
-%      member(H, T),
-%      remove_duplicates( T, List).
+% 'List is the list after removing duplicates from a list with head H and tail T such that H is a member of T and List is 
+% the result of removing duplicates from the list T'
+remove_duplicates([H | T], List) :-    
+     member(H, T),
+     remove_duplicates( T, List).
 
-% remove_duplicates([H | T], [H|T1]) :- 
-%       \+member(H, T),
-%       remove_duplicates( T, T1).
-      
+% 'The list with head H and tail T1 is the result of removing duplicates from the list with head H and tail T such that H is
+% not a member of list T and T1 is the list after removing duplicates from list T'
+remove_duplicates([H | T], [H|T1]) :- 
+      \+member(H, T),
+      remove_duplicates( T, T1).
+
+
 % 1. set_union/3 - if X and Y are sets in list form, then Z is the union of X and Y
 % A union is a set of elements such that each element exists in at least one set
 set_union([],[],[]).
+set_union([X],[],[X]).
+set_union([],[Y],[Y]).
+set_union([X],[Y],[X,Y]) :-
+    different(X,Y).
 
-set_union([], [Y|TailY], [Y|UnionTail]) :-
-    \+member(Y,UnionTail),
-    set_union(TailY,UnionTail).
+% 'UnionList is the list that represents the set union of ListX and ListY such that DuplicatedUnionList is the result of appending
+% ListX and ListY and Union list is the result of removing duplicates from DuplicatedUnionList'
+set_union(ListX,ListY,UnionList) :-
+    append(ListX,ListY,DuplicatedUnionList),
+    remove_duplicates(DuplicatedUnionList,UnionList).
 
-set_union([],[Y|TailY],UnionList) :-
-    member(Y, UnionList),
-    set_union([],TailY,UnionList).
 
-set_union([X|TailX], [], [X|UnionTail]) :- 
-    \+member(X,UnionTail),
-    set_union(TailX,[],UnionTail).
-
-set_union([X|TailX],[],UnionList) :-
-    member(X, UnionList),
-    set_union(TailX,[],UnionList).
-
-set_union([X|TailX], [X|TailY], [X|UnionTail]) :-
-    \+member(X,UnionTail),
-    set_union(TailX, TailY, UnionTail).
-
-set_union([X|TailX], [Y|TailY], [X,Y|UnionTail]) :-
-    different(X,Y),
-    \+member(X, UnionTail),
-    \+member(Y, UnionTail),
-    set_union(TailX, TailY, UnionTail).
-
-set_union([X|TailX], [Y|TailY], [X|UnionTail]) :-
-    different(X,Y),
-    \+member(X, UnionTail),
-    member(Y, UnionTail),
-    set_union(TailX, TailY, UnionTail).
-
-set_union([X|TailX], [Y|TailY], [Y|UnionTail]) :-
-    different(X,Y),
-    member(X, UnionTail),
-    \+member(Y, UnionTail),
-    set_union(TailX, TailY, UnionTail).
-
-% [a,b,c] [b,c,d]
-% X=a,Y=b
-% unionList = [a,b|tail1]
-
-% [b,c] [c,d]
-% X=b,Y=c
-% tail1=[c|tail2]
-
-% [c] [d]
-% X=c,Y=d
