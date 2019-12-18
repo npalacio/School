3e331393da9e126b84eb3ef4803b9f094ffb8b4c
Nick Palacio <nick.palacio@fcsamerica.com>
2019-12-17 18:17:14 -0600

    did Q6

--- AdvancedConceptsInProgrammingLanguages/FinalExam/Q6.rkt
+++ AdvancedConceptsInProgrammingLanguages/FinalExam/Q6.rkt
@@ -3,3 +3,7 @@
 (define (factorial N) (cond
                         [(< N 2) 1]
                         [else (* N (factorial (- N 1)))]))
+(define (sum-digits N acc) (cond
+                             [(< N 10) (+ N acc)]
+                             [else (sum-digits (floor (/ N 10)) (+ (modulo N 10) acc))]))
+(define (f N) (sum-digits (factorial N) 0))
